8181a220e383a0a78f5560075d38998519839841
Kiritsugucarl <bluesniper017@gmail.com>
2022-12-10 13:11:49 +0800

    progressive diffuclty addition

--- bird_in_the_space_game/game.py
+++ bird_in_the_space_game/game.py
@@ -125,6 +125,7 @@ while running:
                 y_positions = []
                 score = 0
                 pygame.mixer.music.play()
+                obstacle_speed = 3
                 active = True
                 
     # bound checking
@@ -143,6 +144,7 @@ while running:
                 obstacles.append(random.randint(obstacles[-1] + 280, obstacles[-1] + 320))
                 y_positions.append(random.randint(0, 300))
                 score += 1
+                obstacle_speed += 0.1
     
     if active:
         score_text = font.render(f'Score : {score}', True, white)
